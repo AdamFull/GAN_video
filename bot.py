66de688e9742f6816fafcae647b58877049d16fa
AdamFull <logotipick@gmail.com>
2020-02-19 15:06:22 +0300

    ...

--- bot.py
+++ bot.py
@@ -5,10 +5,11 @@ from multiprocessing import Process, Queue, Pool
 
 random.seed(10)
 
+width, heigth = 64, 64
 TOKEN = "1078723988:AAGEqQnbEKEkeRenB09SXeDZmWeJyIsCN88"
 keyboard1 = telebot.types.ReplyKeyboardMarkup()
 keyboard1.row('тян')
-proxy = telebot.apihelper.proxy = {'https': 'socks5h://54.38.195.161:49985',}
+proxy = telebot.apihelper.proxy = {'https': 'socks5h://144.76.99.207:84',}
 
 self_path = os.path.dirname(os.path.abspath(__file__))
 temp_path = os.path.join(self_path, "temp")
@@ -17,6 +18,8 @@ if not os.path.exists(temp_path):
     os.makedirs(temp_path)
 
 bot = telebot.TeleBot(TOKEN)
+
+generator = GAN(buff_size=10035, batch_size=16, epochs=5000, imgs_size=(width, heigth))
 
 @bot.message_handler(commands=['start', 'help'])
 def start_handler(message):
@@ -36,7 +39,5 @@ def msg_handler(message):
     else:
         bot.send_message(message.chat.id, "Каво? Ты что дядя хочешь от дедушки. Я знаю команды: Тян мне, тян мне, тян, Тян")
 
-try:
-    bot.polling(none_stop=True)
-except Exception as e:
-    print(e)
+
+bot.polling(none_stop=True)
